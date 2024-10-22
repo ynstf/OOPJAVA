ef073dd0f70d5152df69fc600e7ae23f99792074
YONES <107154559+ynstf@users.noreply.github.com>
2024-10-22 14:44:07 +0100

    ex3 , add test

---
+++ TP1/Exercice 3/src/Test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Str content = new Str();
+
+		content.input();
+		content.show();
+		content.reverse();
+		content.countWords();
+
+    }
+}
